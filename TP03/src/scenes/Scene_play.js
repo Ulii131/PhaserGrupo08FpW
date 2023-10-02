d5f07b09ab4c2d23389470e2c405ba5f0e0f86f5
jorgelinaaC <jorequisde15@gmail.com>
2023-10-02 23:55:20 +0000

    agregado de estrellas, bombas y puntaje

--- TP03/src/scenes/Scene_play.js
+++ TP03/src/scenes/Scene_play.js
@@ -1,44 +1,109 @@
 class Scene_play extends Phaser.Scene{
 
+
   constructor(){
     super('Scene_play');
   }
 
 
   preload(){
-    
+    this.load.image('star', '../assets/star.png');
+    //this.load.image('bomb', '../assets/bomb.png');
   }
 
-  create(){
-    this.add.image(400,300,'sky');
-    //CREACION DE LAS PLATAFORMAS
 
+  create()
+  {
+    
+    this.add.image(400,300,'sky');
+
+    //CREACION DE LAS PLATAFORMAS
    let platforms = this.physics.add.staticGroup();
 
    platforms.create(400, 568, 'platform').setScale(2).refreshBody();
-
    platforms.create(600, 400, 'platform');
    platforms.create(50, 250, 'platform');
    platforms.create(750, 220, 'platform');
 
-  this.add.image (100, 100, 'star');
-  this.player = this.physics.add.sprite(100, 100, 'dude');
-
-  this.player.setBounce(0.3);
-  this.player.setCollideWorldBounds(true);
-
-
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
+    //CREACION DE LAS ESTRELLAS
+    let stars = this.physics.add.group({
+      key: 'star',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 }
     });
 
+    stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    //puntuacion que se mostrara en pantalla
+    let score = 0;
+    let scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+
+    // CREACIÓN DE LAS BOMBAS
+    let bombs = this.physics.add.group({
+      key: 'bomb',
+      repeat: 4, 
+      setXY: { x: 170, y: 0, stepX: 100 } 
+    });
+
+    bombs.children.iterate(function (child) {
+      child.setBounce(0.5, 1); 
+    });
+
+    //funcion recolectar estrellas + puntuacion +  +posicion random bombas 
+    function collectStar (player, star)
+    {
+      star.disableBody(true, true);
+
+      score += 10;
+      scoreText.setText('Score: ' + score);
+
+      if (stars.countActive(true) === 0)
+    {
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    }
+
+    }
+
+    function hitBomb(player, bomb) {
+      this.physics.pause();
+    
+      player.setTint(0xff0000);
+      player.anims.play('turn');
+    
+      gameOver = true;
+    }    
+
+   //CREACION JUGADOR
+    this.player = this.physics.add.sprite(100, 100, 'dude');
+    this.player.setBounce(0.3);
+    this.player.setCollideWorldBounds(true);
+
+    //CREACION ANIMACION
     this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'dude', frame: 4 } ],
-    frameRate: 20
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+      });
+
+      this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'dude', frame: 4 } ],
+      frameRate: 20
     });
 
     this.anims.create({
@@ -48,10 +113,18 @@ class Scene_play extends Phaser.Scene{
     repeat: -1
     });
 
+    //colision jugador-platafotmas
     this.physics.add.collider(this.player, platforms);
-
+    // Agregar colisión entre jugador y estrellas
+    this.physics.add.collider(stars, platforms);
+    //colision jugador-estrellas
+    this.physics.add.overlap(this.player, stars, collectStar, null, this);
     this.cursors = this.input.keyboard.createCursorKeys();
+    //colision bombas-platafotmas
+    this.physics.add.collider(bombs, platforms);
+    this.physics.add.collider(this.player, bombs, hitBomb, null, this);
   }
+  
 
   update() {
     if (this.cursors.left.isDown) {
@@ -70,13 +143,6 @@ class Scene_play extends Phaser.Scene{
     this.player.setVelocityY(-330);
     }
     }
-
-    
-
-
-
-
-
 }
 
 export default Scene_play;
